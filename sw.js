4b674c7c19d38171c27c64c42b455ba21c8f53a4
nico6286 <160365328+nico6286@users.noreply.github.com>
2026-06-05 18:02:05 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* ── Budget Couple — Service Worker ── */
-const CACHE_NAME = "budget-couple-v8"; // Changé en v8 pour forcer le nettoyage de l'ancien index.html buggé
+const CACHE_NAME = "budget-couple-v9"; // Changé en v9 pour forcer le nettoyage de l'ancien index.html buggé
 const ASSETS = ["./index.html", "./manifest.json"];
 
 self.addEventListener("install", e => {
